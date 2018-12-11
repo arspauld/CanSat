cd9a7b65e7cee852e20092498559f1cad8b630e5
Patrick Smith <pjsmith29510@gmail.com>
2018-12-11 10:56:43 -0500

    Made changes from a Python COnsole to a List and push button.

--- Software/Ground Station/main.py
+++ Software/Ground Station/main.py
@@ -4,6 +4,7 @@ from pyqtgraph.Qt import QtGui, QtCore
 from pyqtgraph import console
 from pyqtgraph.dockarea import *
 from realplot import RealTimePlot
+import random
 
 app = QtGui.QApplication([])                    # Starts the GUI application
 win = QtGui.QMainWindow()                       # Creates the main storage for docks
@@ -14,16 +15,32 @@ win.setWindowTitle("Ground Station Control")    # Names the window
 
 ## Creates two Docks and inserts them into the area
 d1 = Dock("Plot")
-d2 = Dock("Console")
+d2 = Dock("Messages and Communication")
 area.addDock(d1, 'left')
 area.addDock(d2, 'right')
 
 ## Add Widgets to the docks
-plot = RealTimePlot(name="Example", x_val=np.arange(0,100), y_val=np.random.normal(0, 100, size=100))
+plot = RealTimePlot(name="Example")
 d1.addWidget(plot.plot)
 
-console = console.ConsoleWidget()
-d2.addWidget(console)
+w = QtGui.QWidget()
+btn = QtGui.QPushButton('press me')
+text = QtGui.QLineEdit('enter text')
+listw = QtGui.QListWidget()
+layout = QtGui.QGridLayout()
+w.setLayout(layout)
+d2.addWidget(w)
+
+layout.addWidget(btn, 2, 0)   # button goes in upper-left
+layout.addWidget(text, 1, 0)   # text edit goes in middle-left
+layout.addWidget(listw, 0, 0)  # list widget goes in bottom-left
 
 win.show()                                      # Shows window
+
+## Real Time Graphing
+points = 100
+for i in range(points):
+    plot.add(i, random.randint(0, points))
+    app.processEvents()
+
 app.exec_()                                     # Executes application
