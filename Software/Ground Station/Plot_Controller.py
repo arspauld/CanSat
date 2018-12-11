09ce0ef0abf986da5978927d473733b4b1d828fe
Patrick Smith <pjsmith29510@gmail.com>
2018-12-10 19:23:50 -0500

    Working towards a class for realtime plotting

---
+++ Software/Ground Station/Plot_Controller.py
@@ -0,0 +1,5 @@
+"""Plot Controller Class used to handle
+   plotting duties for Ground Station"""
+import numpy as np
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
---
+++ Software/Ground Station/main.py
@@ -0,0 +1,21 @@
+import numpy as np
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+from realtimeplot import PlotManager
+import time
+
+length = 1
+costs  = np.arange(length)
+
+plt_mgr = PlotManager(
+	title="plots",
+	nline=3)
+
+for i in range(length):
+	cost = costs[i]
+	plt_mgr.add("cost", cost)
+	plt_mgr.add("time", time.time())
+	plt_mgr.add("time2", time.time())
+	plt_mgr.update()
+
+plt_mgr.close()
