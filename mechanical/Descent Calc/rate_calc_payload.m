50924ae34f2e7b5f8f054bb551b62718f761d8b3
Patrick Smith <pjsmith29510@gmail.com>
2019-01-27 16:26:24 -0600

    Update rate_calc_payload.m

--- mechanical/Descent Calc/rate_calc_payload.m
+++ mechanical/Descent Calc/rate_calc_payload.m
@@ -13,6 +13,7 @@ coef = polyfit(alpha, c_drag, 7);
 c_d = @(t) coef(1)*t.^7+coef(2)*t.^6+coef(3)*t.^5+coef(4)*t.^4+coef(5)*t.^3+coef(6)*t.^2+coef(7)*t+coef(8);
 x = linspace(-8, 16);
 
+figure(1);
 subplot(221);
 plot(alpha, c_lift, x, c_l(x));
 title('\alpha versus C_L')
@@ -41,7 +42,7 @@ L = 207;        % mm
 m = .4;         % kg
 g = 9.81;       % m / s^2
 rho = 1.16;     % kg / m^3
-V_y = 15;       % m / s
+V_y = 10;       % m / s
 
 
 phi = @(h) 10 * h / L - 10; % degrees
@@ -51,7 +52,7 @@ w = @(h) (w_f-w_i)*h/L + w_i;    % mm
 
 
 dh = .01;
-spin = 0:.1:50;
+spin = 35:.1:55;
 rads = 2 * pi * spin;
 lift_comp(1, length(spin)) = 0;
 drag_comp(1, length(spin)) = 0; 
@@ -62,26 +63,27 @@ for i = 0:dh:L
     alpha = p - theta; % degrees
     dA = w(i) * dh * .001^2; % m^2
     V_sq = V_x.^2 + V_y^2;
+    V = sqrt(V_sq);
     
-    lift_comp(1, :) = lift_comp(1, :) + rho * dA / 2 * V_sq .* c_l(alpha) .* cosd(theta);
+    lift_comp(1, :) = lift_comp(1, :) + rho * dA / (2 * V_y) * V.^3 .* c_d(alpha);
     drag_comp(1, :) = drag_comp(1, :) + rho * dA / 2 * V_sq .* c_d(alpha) * V_y .* cosd(theta) ./ V_x;
 end
 
 weight = m * g * ones(1,length(lift_comp));
-net_vert = 4 * lift_comp + 4 * drag_comp - weight;
+net_vert = 4 * lift_comp - weight;
+
+
+% subplot(131);
+% plot(spin*60, lift_comp);
+% grid on;
+% xlabel('Spin Rate [rpm]'); ylabel('Lift [N]');
+% 
+% subplot(132);
+% plot(spin*60, drag_comp);
+% grid on;
+% xlabel('Spin Rate [rpm]'); ylabel('Drag [N]');
 
 figure(2);
-subplot(131);
-plot(spin*60, lift_comp);
-grid on;
-xlabel('Spin Rate [rpm]'); ylabel('Lift [N]');
-
-subplot(132);
-plot(spin*60, drag_comp);
-grid on;
-xlabel('Spin Rate [rpm]'); ylabel('Drag [N]');
-
-subplot(133);
 plot(spin*60, net_vert);
 grid on;
 xlabel('Spin Rate [rpm]'); ylabel('Net Force [N]');
